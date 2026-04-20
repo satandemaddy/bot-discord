74e5e5870f7b5ce3a5af3e31288bd1bfc02021f5
Satandemaddy <lgxcbolts@gmail.com>
2026-04-19 22:46:43 -0600

    clean sin token

---
+++ bot.js
@@ -0,0 +1,31 @@
+const { Client, GatewayIntentBits } = require('discord.js');
+const { joinVoiceChannel } = require('@discordjs/voice');
+
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildVoiceStates
+  ]
+});
+
+client.once('ready', () => {
+  console.log(`Bot listo como ${client.user.tag}`);
+  client.user.setActivity('Manteniendo VC activo :loud_sound:');
+});
+
+// Cuando alguien entra a un VC
+client.on('voiceStateUpdate', (oldState, newState) => {
+  if (!oldState.channelId && newState.channelId) {
+    const channel = newState.channel;
+
+    joinVoiceChannel({
+      channelId: channel.id,
+      guildId: channel.guild.id,
+      adapterCreator: channel.guild.voiceAdapterCreator,
+    });
+
+    console.log(`Me metí a ${channel.name}`);
+  }
+});
+
+client.login('process.env.TOKEN');
