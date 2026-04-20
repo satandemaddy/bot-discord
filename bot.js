444d069ad7765a2a4f118ba92318fc7fb8dabea3
Satandemaddy <lgxcbolts@gmail.com>
2026-04-19 23:54:49 -0600

    fix vc crash

--- bot.js
+++ bot.js
@@ -1,33 +1,28 @@
-console.log("ARRANCANDO BOT...");
-
-require('dotenv').config();
-
-const { Client, GatewayIntentBits } = require('discord.js');
-const { joinVoiceChannel } = require('@discordjs/voice');
-
-const client = new Client({
-    intents: [
-        GatewayIntentBits.Guilds,
-        GatewayIntentBits.GuildVoiceStates
-    ]
-});
-
 client.once('ready', async () => {
     console.log(`Bot listo como ${client.user.tag}`);
 
-    const channel = client.channels.cache.get("1476366375218843713");
+    try {
+        const channel = await client.channels.fetch("1476366375218843713");
 
-    if (!channel) return console.log("No encontré el canal");
+        if (!channel) {
+            console.log("No encontré el canal");
+            return;
+        }
 
-    joinVoiceChannel({
-        channelId: channel.id,
-        guildId: channel.guild.id,
-        adapterCreator: channel.guild.voiceAdapterCreator,
-    });
+        if (!channel.isVoiceBased()) {
+            console.log("El canal no es de voz");
+            return;
+        }
 
-    console.log("Entré al VC automáticamente");
+        joinVoiceChannel({
+            channelId: channel.id,
+            guildId: channel.guild.id,
+            adapterCreator: channel.guild.voiceAdapterCreator,
+        });
+
+        console.log("Entré al VC automáticamente");
+
+    } catch (error) {
+        console.error("Error al entrar al VC:", error);
+    }
 });
-
-client.login(process.env.TOKEN)
-    .then(() => console.log("LOGIN OK"))
-    .catch(err => console.error("ERROR LOGIN:", err));
